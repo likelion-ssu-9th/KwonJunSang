48a01e0fbcb9a31615f124d2ca62bce180656333
JunsangKwon <wnstkdsun97@naver.com>
2021-05-16 17:40:06 +0900

    Session4 Django Assignment

---
+++ Django/blogproject/blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    writer = models.CharField(max_length=100)
+    pub_data = models.DateTimeField()
+    body = models.TextField() # 제한이 없게 해줌
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
