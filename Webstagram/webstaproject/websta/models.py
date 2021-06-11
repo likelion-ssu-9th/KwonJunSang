7fab24b1f4bce36a9eeff5fbc1447099b107a1f8
JunsangKwon <wnstkdsun97@naver.com>
2021-06-11 17:21:02 +0900

    Create, Update, Delete 구현

--- Webstagram/webstaproject/websta/models.py
+++ Webstagram/webstaproject/websta/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-
 class Websta(models.Model):
     writer = models.CharField(max_length=100)
     image = models.ImageField(upload_to="images/", blank=True, null=True)
--- Webstagram/webstaproject/websta/views.py
+++ Webstagram/webstaproject/websta/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Websta
 
 # Create your views here.
@@ -19,3 +19,34 @@ def signup(request):
 
 def profile(request):
     return render(request, 'profile.html')
+
+
+def new(request):
+    return render(request, 'new.html')
+
+
+def create(request):
+    new_post = Websta()
+    new_post.writer = request.POST['writer']
+    new_post.image = request.FILES['image']
+    new_post.save()
+    return redirect('feed')
+
+
+def edit(request, id):
+    edit_post = Websta.objects.get(id=id)
+    return render(request, 'edit.html', {'post': edit_post})
+
+
+def update(request, id):
+    update_post = Websta.objects.get(id=id)
+    update_post.writer = request.POST['writer']
+    update_post.image = request.FILES['image']
+    update_post.save()
+    return redirect('feed')
+
+
+def delete(request, id):
+    delete_post = Websta.objects.get(id=id)
+    delete_post.delete()
+    return redirect('feed')
